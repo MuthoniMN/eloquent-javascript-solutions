dd5a1588a746021648def7fbf284587e187e2008
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-06-13 16:27:25 +0300

    correcting arraayToList

--- chapter-4-exercises/a-list/script.js
+++ chapter-4-exercises/a-list/script.js
@@ -2,7 +2,7 @@
 function arrayToList(array) {
     let list = null
 
-    for (let i = array.length - 1; i > 0; i++) {
+    for (let i = array.length - 1; i >= 0;  i--) {
         const element = array[i];
 
         list = {
